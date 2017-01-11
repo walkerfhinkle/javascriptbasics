e7f920f07f1a6a5c537331957a4f339b3425894a
King James <King James>
2017-01-11 17:36:34 -0500

    added javascript basics

---
+++ 0.08_switch.js
@@ -0,0 +1,38 @@
+// Switch Cases
+
+/*
+switch Casesswitch(expression) {
+	case n:
+		code block
+		break;
+	case n:
+		code block
+		break;
+	default:
+		default code block (else portion of switch)
+}
+*/
+
+var marginofcoltsuperbowlwin = 17;
+var result
+
+switch (marginofcoltsuperbowlwin) {
+	case 3:
+		result = "Cool, that will be a good game"
+		break;
+	case 7:
+		result = "I like it. Just close enough"
+		break;
+	case 10:
+		result = "That's what I'm talking about"
+		break;
+	case 17:
+		result = "Sweet, we're awesome"
+		break;
+	default:
+		result = "mmhmm gettin' there"
+}
+console.log("switch result: " + result)
+
+
+
